77c31766c1ec002e78c52a203406b59b92e0760d
Eduard Vallve <eduard.vallve@netcentric.biz>
2022-10-19 13:04:01 +0200

    PF - New Passions component is created

---
+++ htdocs/components/passions/passions.php
@@ -0,0 +1,6 @@
+<section class="passions">
+  <main class="passions__main">
+    <h2 class="passions__name">My passions</h2>
+    <span class="passions__description">What I want to share with you</span>
+  </main>
+</section>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // Index page. This is the first page that the web server finds.
 $pageInclude = 'index';
 $pageTitle = 'Hi!';
-$pageComponents = ['brief'];
+$pageComponents = ['brief','passions'];
 
 // Output the page.
 include 'includes/page-output.inc.php';
