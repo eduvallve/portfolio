429681fd20d1c8b396606187b04d4b83d7989790
Eduard Vallve <eduard.vallve@netcentric.biz>
2022-12-12 16:47:56 +0100

    PF - Passions has been removed

--- htdocs/components/passions/passions.php
+++
@@ -1,6 +0,0 @@
-<section class="passions">
-  <main class="passions__main">
-    <h2 class="passions__name">My passions</h2>
-    <span class="passions__description">What I want to share with you</span>
-  </main>
-</section>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // Index page. This is the first page that the web server finds.
 $pageInclude = 'index';
 $pageTitle = 'Hi!';
-$pageComponents = ['brief','passions'];
+$pageComponents = ['brief'];
 
 // Output the page.
 include 'includes/page-output.inc.php';
