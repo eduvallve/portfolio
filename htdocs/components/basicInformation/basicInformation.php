b5e86be8b1425f539ab209221177e7e6984e1a80
Eduard Vallve <eduard.vallve@netcentric.biz>
2023-02-23 14:31:25 +0100

    PF - Basic information component created with basic data in English

---
+++ htdocs/components/basicInformation/basicInformation.php
@@ -0,0 +1,18 @@
+<section class="basicInformation">
+  <main class="basicInformation__main">
+    <div class="cols">
+      <div class="col-1"><img class="profilePic" src="<?php echo $uri."/uploads/2023/02/profilePic.svg";?>" alt="Profile Picture"></div>
+      <div class="col-3">
+        <h2 class="basicInformation__name">Eduard Vallvé</h2>
+        <p class="basicInformation__slogan">I <span class="heart">&#10084;</span> user interfaces</p>
+        <p class="basicInformation__description"><?php echo (intval(date("Y")) - 2016); ?> years of experience working with both small and big companies.</p>
+        <a class="btn btn__secondary icon icon__linkedin" href="#" target="_blank">My LinkedIn</a>
+        <a class="btn btn__secondary icon icon__resumee" href="#" target="_blank">My Resumee</a>
+      </div>
+      <div class="col-4">
+        <p class="basicInformation__studies icon icon__graduate">Degree in Multimedia (UPC).</p>
+        <p class="basicInformation__studies icon icon__master">Master degree in UX & Interface design (UOC).</p>
+      </div>
+    </div>
+  </main>
+</section>
