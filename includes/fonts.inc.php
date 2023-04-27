a6b94ca985fc96f731c9391bea8600404c2052a8
Eduard Vallve <eduard.vallve@netcentric.biz>
2023-04-27 09:09:45 +0200

    PF - Minimize the amount of css files in the page head

--- includes/fonts.inc.php
+++
@@ -1,6 +0,0 @@
-<style>
-    @font-face {
-        font-family: "Neoneon";
-        src: url("<?php echo $uri; ?>/htdocs/fonts/Neoneon.otf");
-    }
-</style>
--- includes/header.inc.php
+++ includes/header.inc.php
@@ -7,18 +7,17 @@
 <meta name="description" content="Eduard Vallvé :: Portfolio :: <?php echo $pageTitle; ?>">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link defer rel="shortcut icon" href="<?php echo $uri.'/htdocs/'; ?>favicon.ico" type="image/png" />
-<?php include 'fonts.inc.php'; ?>
-<link defer rel="stylesheet" href="<?php echo $uri.'/htdocs/common/'; ?>style.css">
 <!-- Styles for components of this page -->
+<style type="text/css">
 <?php
+include 'htdocs/common/style.css';
 if ($pageComponents) {
     $pageComp = array_unique($pageComponents);
-    foreach ($pageComp as $component) { ?>
-        <style type="text/css"><?php
-            include 'htdocs/components/'.$component.'/'.$component.'.css';
-        ?></style>
-    <?php }
+    foreach ($pageComp as $component) {
+        include 'htdocs/components/'.$component.'/'.$component.'.css';
+    }
 }
 ?>
+</style>
 </head>
 <body onclick="void(0);" class="page__<?php echo $pageInclude; ?>">
