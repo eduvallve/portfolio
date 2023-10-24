92b1b35e8fd7daaeb7f7268770eccf3d45128b7e
Eduard Vallvé <66818781+eduvallve@users.noreply.github.com>
2023-10-24 09:03:41 +0200

    PF - Connect current structure with projects content page

--- htdocs/pages/index.php
+++ htdocs/pages/index.php
@@ -1,8 +1,13 @@
 <?php
 if ($pageComponents) {
     foreach ($pageComponents as $component) {
-        include "htdocs/components/".$component."/lang/".$component.".".$lang.".php";
+        include 'htdocs/components/'.$component.'/lang/'.$component.'.'.$lang.'.php';
         include 'htdocs/components/'.$component.'/'.$component.'.php';
+    }
+} else if ($pageContent) {
+    foreach ($pageContent as $content) {
+        include 'htdocs/pages/project/'.$content.'/lang/'.$content.'.'.$lang.'.php';
+        include 'htdocs/pages/project/'.$content.'/'.$content.'.php';
     }
 }
 ?>
